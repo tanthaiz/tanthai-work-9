7fb8807d8a17d399223ab56f7d65998470644a1a
tanthaiz <139226043+tanthaiz@users.noreply.github.com>
2024-01-24 16:04:02 +0700

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,15 @@
+print("ข้อ 1")
+for i in range(1,16,2):
+    print (i)
+
+print("ข้อ 2")
+for i in range(2,18,3):
+    print (i)
+
+print("ข้อ 3")
+for i in range(30,-31,-10):
+    print (i)
+
+print("ข้อ 4")
+for i in range(15,56,8):
+    print (i)
